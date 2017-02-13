c4c7c08cbdca6cc639c1c539b8c43cdd4d04c147
avaret <avaret@gmail.com>
2017-02-13 16:18:43 +0100

    Essai d'ajout d'un projet Java de test depuis Eclipse

---
+++ testproject/src/testproject/maintest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testproject;
+
+/**
+ * @author avaret
+ *
+ */
+public class maintest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world !");
+	}
+
+}
